ec731c2cf4c67fa45b500f7275ce3a60aa6be7bc
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-25 12:41:35 +0530

    Update welcomeJob.groovy

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,9 +1,12 @@
+import groovy.yaml.YamlSlurper
+
 def call(String name = 'User') {
  echo "Welcome, ${name}."
 }
 
 def testFun(String name = 'User') {
- Map pipelineCfg = readYaml file: "C:/Users/66480/.jenkins/workspace/shared-lib-spec/build.yml"
+ def pipelineCfg = new YamlSlurper().parseText("C:/Users/66480/.jenkins/workspace/shared-lib-spec/build.yml")
+ //Map pipelineCfg = readYaml file: "C:/Users/66480/.jenkins/workspace/shared-lib-spec/build.yml"
  echo "Test Welcome, ${name}."
  echo "Test Welcome, ${pipelineCfg}."
 }
