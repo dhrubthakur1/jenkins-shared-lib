748df166c4638e865e7d766a4a24df7dc5cc93cf
dhrubthakur1 <dhrubthakur@gmail.com>
2021-12-25 12:35:13 +0530

    Update welcomeJob.groovy

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -3,5 +3,7 @@ def call(String name = 'User') {
 }
 
 def testFun(String name = 'User') {
+ Map pipelineCfg = readYaml(file: "http://localhost:8080/job/shared-lib-spec/47/execution/node/4/ws/build.yml")
  echo "Test Welcome, ${name}."
+ echo "Test Welcome, ${pipelineCfg}."
 }
